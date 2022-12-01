9dbd46b9365bafaa98351c84effb6e79c1b56498
SUCCmijnbolle <aidenhoek@gmail.com>
2022-12-01 14:42:27 +0100

    Update ResetIsJumping.cs

--- Angels descent/Assets/ResetIsJumping.cs
+++ Angels descent/Assets/ResetIsJumping.cs
@@ -16,11 +16,11 @@ public class ResetIsJumping : StateMachineBehaviour
     //    
     //}
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("isJumping", false);
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
